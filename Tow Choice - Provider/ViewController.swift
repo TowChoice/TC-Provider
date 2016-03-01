e2249be339e1430018dbf43d7eb94ab287181f01
oo-di-lolly <alexis0716@gmail.com>
2016-03-01 00:06:19 -1000

    Change to UIWebView

--- Tow Choice - Provider/ViewController.swift
+++ Tow Choice - Provider/ViewController.swift
@@ -9,24 +9,19 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet weak var loadingView: UIActivityIndicatorView!
     @IBOutlet weak var loadingImage: UIImageView!
+    lazy var webView = UIWebView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        let webView: WKWebView = WKWebView(frame: CGRectMake(0, 0, view.bounds.width, view.bounds.height))
-        webView.navigationDelegate = self
+        webView.frame = CGRectMake(0, 0, view.bounds.width, view.bounds.height)
+        webView.delegate = self
         view.addSubview(webView)
         webView.hidden = true
         view.autoresizesSubviews = true
-//        let url = NSURL(string: "https://google.com")!
-
-        let url = NSURL(string: "http://live.towchoice.com/provider/#signinpage")!
+        let url = NSURL(string: "http://live.towchoice.com/provider/")!
         webView.loadRequest(NSURLRequest(URL: url))
-//        webView.loadHTMLString("", baseURL:NSURL(string: "http://live.towchoice.com/provider"))
-
-//        webView.load
-
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -41,32 +36,17 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
-        print(navigationAction.request.URL)
-        print(navigationAction.navigationType)
-        decisionHandler(WKNavigationActionPolicy.Allow)
-
-    }
-    func webView(webView: WKWebView, didReceiveAuthenticationChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
-        <#code#>
-    }
-    func webView(webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    func webViewDidStartLoad(webView: UIWebView) {
         loadingView.startAnimating()
         loadingView.hidden = false
-        webView.hidden = true
         NSLog("WebView started to load")
     }
     
-    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+    func webViewDidFinishLoad(webView: UIWebView) {
         loadingView.stopAnimating()
         loadingView.hidden = true
         loadingImage.hidden = true
         webView.hidden = false
-    }
-    
-    func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
-        let alertVC = UIAlertController(title: "Request Failed", message: error.localizedDescription, preferredStyle: .Alert)
-        self.presentViewController(alertVC, animated: true, completion: nil)
     }
 
 }
